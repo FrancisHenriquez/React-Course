8cb1aa8800ff42f6872a554e1abd63d22547cdf8
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-07 12:08:37 -0400

    funciones

---
+++ src/05-funciones.js
@@ -0,0 +1,19 @@
+// const saludar =function  (nombre){
+//     return `Hola, ${nombre} como estas?`
+// }
+
+// const saludar2 = (nombre) => {
+//     return `Hola, ${nombre} como estas?`
+// }
+
+const saludar3 = (nombre) => `Hola, ${nombre} como estas?`
+
+// console.log(saludar2 ('Vegueta'))
+console.log(saludar3 ('Goten'))
+
+const getUser = ()=> ({
+    uid: '20165833',
+    username: 'KeepitMello'
+});
+
+console.log(getUser);
